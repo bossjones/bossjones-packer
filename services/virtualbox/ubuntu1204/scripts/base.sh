943dc444cf95bedc595cdebfc450b6862d8eb14f
Malcolm Jones <malcolm@adobe.com>
2014-09-18 18:52:17 -0400

    feature: added zerodisk.sh script along with others to install correct packages @minor

--- services/virtualbox/ubuntu1204/scripts/base.sh
+++ services/virtualbox/ubuntu1204/scripts/base.sh
@@ -3,3 +3,14 @@
 cd /etc/init.d && ./xively_monitoring stop
 \rm -rfv /etc/init.d/xively_monitoring*
 \rm -rfv /var/log/*.log.*
+
+apt-get -y update
+apt-get -y upgrade
+apt-get -y install curl
+# Ensure NFS mounts work properly
+apt-get -y install nfs-common
+apt-get clean
+
+date > /etc/vagrant_box_build_time
+
+apt-get install -y build-essential dkms rubygems lxc-docker
---
+++ services/virtualbox/ubuntu1204/scripts/ruby.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+apt-get update
+
+apt-get install ruby1.9.1 ruby1.9.1-dev \
+  rubygems1.9.1 irb1.9.1 ri1.9.1 rdoc1.9.1 \
+  build-essential libopenssl-ruby1.9.1 libssl-dev zlib1g-dev
+
+update-alternatives --install /usr/bin/ruby ruby /usr/bin/ruby1.9.1 400 \
+         --slave   /usr/share/man/man1/ruby.1.gz ruby.1.gz \
+                        /usr/share/man/man1/ruby1.9.1.1.gz \
+        --slave   /usr/bin/ri ri /usr/bin/ri1.9.1 \
+        --slave   /usr/bin/irb irb /usr/bin/irb1.9.1 \
+        --slave   /usr/bin/rdoc rdoc /usr/bin/rdoc1.9.1
+
+# choose your interpreter
+# changes symlinks for /usr/bin/ruby , /usr/bin/gem
+# /usr/bin/irb, /usr/bin/ri and man (1) ruby
+update-alternatives --config ruby
+update-alternatives --config gem
+
+# now try
+ruby --version
